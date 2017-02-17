a3676b2fdfa5fac3e3880aca444f7c3203088927
washingtondeveloper <washingtonprogramador@gmail.com>
2017-02-17 12:03:59 -0200

    library .NET 1.0.0

---
+++ GeneratorOfContext/Framework/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorOfContext.Framework
+{
+    public interface IRepository<T> where T : class
+    {
+        void Save(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+        T GetOneId(long id);
+        T GetOne(T obj);
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ GeneratorOfContext/Framework/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorOfContext.Framework
+{
+    public class Repository<T, C> : IRepository<T> where T : class where C : DbContext
+    {
+        protected C contexto;
+
+        public Repository()
+        {
+            contexto = (C)Activator.CreateInstance(typeof(C));
+        }
+
+        public void Delete(T obj)
+        {
+            contexto.Set<T>().Remove(obj);
+            contexto.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return contexto.Set<T>().ToList();
+        }
+
+        public T GetOne(T obj)
+        {
+            return contexto.Set<T>().Find(obj);
+        }
+
+        public T GetOneId(long id)
+        {
+            return contexto.Set<T>().Find(id);
+        }
+
+        public void Save(T obj)
+        {
+            contexto.Set<T>().Add(obj);
+            contexto.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            contexto.Entry(obj).State = EntityState.Modified;
+            contexto.SaveChanges();
+        }
+    }
+}
